a35a9c452b87cbb59b8b6cd479780ec5ebf7fb31
andrecn13 <cnandre.nunes@gmail.com>
2017-06-07 01:30:45 -0300

    config migration multiple schemas

---
+++ src/main/resources/db/migration/public/V1__tabelas_iniciais.sql
@@ -0,0 +1,31 @@
+CREATE TABLE TBL_ROLE(
+	ID			serial PRIMARY KEY,
+    STR_ROLE	varchar(40) NOT NULL
+);
+
+CREATE TABLE TBL_USUARIO(
+	ID				serial PRIMARY KEY,
+    STR_NOME       	varchar(40) NOT NULL,
+    STR_TENANT 		varchar(40) NOT NULL,
+    NUM_TELEFONE  	varchar(12) NULL,
+    STR_EMAIL       varchar(40) NOT NULL,
+    STR_SENHA  		varchar(40) NOT NULL,
+    BOO_ADMIN  		char(1) NOT NULL DEFAULT 'N'
+);
+
+CREATE TABLE TBL_USUARIO_ROLE(
+    FK_USUARIO integer references TBL_USUARIO (ID),
+    FK_ROLE integer references TBL_ROLE (ID)
+);
+
+CREATE TABLE TBL_CLINICA(
+    ID serial PRIMARY KEY,
+    STR_NOME VARCHAR(500) NOT NULL,
+    NUM_CNPJ BIGINT NOT NULL,
+    BOO_ATIVO BOOLEAN NOT NULL DEFAULT(TRUE)
+);
+
+CREATE TABLE TBL_USUARIO_CLINICA(
+    FK_USUARIO integer references TBL_USUARIO (ID),
+    FK_CLINICA integer references TBL_CLINICA (ID)
+);
